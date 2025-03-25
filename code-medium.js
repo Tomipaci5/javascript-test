f7005e5eaf6086913dfe0c5298afe2f291aa7f4b
Tomáš Jakub <tomas.jakub142@gmail.com>
2025-03-25 08:54:25 +0100

    code-medium.js

--- code-medium.js
+++ code-medium.js
@@ -7,7 +7,16 @@ function capitalizeWord(stringInput, wordIndex) {}
 // 2) Funkce co přijme testScore, a vrátí odpovídající známku podle následující tabulky:
 // 0-50 -> F, 51-60 -> E, 61-70 -> D, 71-80 -> C, 81-90 -> B, 91-100 -> A, pokud je testScore mimo rozsah 0-100, nebo nevalidní hodnota funkce vrátí "INVALID SCORE"
 // například pro 75 bude výsledek "C"
-function transformScoreToGrade(testScore) {}
+function transformScoreToGrade(testScore) {
+if (testScore <= 50) return 'F';
+if (testScore <= 60) return 'E';
+if (testScore <= 70) return 'D';
+if (testScore <= 80) return 'C';
+if (testScore <= 90) return 'B';
+if (testscore <= 100) return 'A';
+}
+
+
 
 // 3) Funkce na výpočet factorialu, pomocí cyklu, vstupem je factorialNumber, výstupem bude výsledek faktoriálu
 // nevalidní vstupem je záporné číslo, nebo nečíslo, v takovém případě funkce vrátí undefined
